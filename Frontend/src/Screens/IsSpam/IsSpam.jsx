3a32f043545f5d08ddd223782236049d24bcc9d1
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2024-03-23 15:42:01 +0530

    HighLight Part Done

--- Frontend/src/Screens/IsSpam/IsSpam.jsx
+++ Frontend/src/Screens/IsSpam/IsSpam.jsx
@@ -5,17 +5,13 @@ import './IsSpam.css'
 import Spam from './Spam'
 import NotSpam from './NotSpam'
 const IsSpam = () => {
+  const str="Subject: You Won! Congratulations! Hello, We are thrilled to inform you that you have been randomly selected as a winner in our exclusive online sweepstakes! You have a chance to claim a FREE brand new smartphone (limited time offer)!To claim your prize, simply click the link below and verify your account information. Act now! Don't miss out on this incredible opportunity www.jitoinam.comCongratulations again!Sincerely,The Devdut Firm Team"
   return (
     <>
-      <Spam />
+      <Spam str={str}  />
       {/* <NotSpam /> */}
     </>
   )
 }
 
-export default IsSpam
-
-
-{/*
-  
-*/}
+export default IsSpam;
--- Frontend/src/Screens/IsSpam/Spam.jsx
+++ Frontend/src/Screens/IsSpam/Spam.jsx
@@ -1,23 +1,55 @@
-import React from 'react'
-import itisSpam from '../../../Assets/itisSpam.png'
-import './IsSpam.css'
+import React from 'react';
+import itisSpam from '../../../Assets/itisSpam.png';
+import './IsSpam.css';
 
-const Spam = () => {
+const Spam = (props) => {
+  console.log(props);
+
+  const mp = new Map([
+    ["randomly", 1],
+    ["sweepstakes", 2],
+    ["act", 3],
+    ["congratulations!", 4],
+    ["phishing", 5],
+    ["urgent", 6],
+    ["limited", 7],
+    ["good", 8],
+    ["wire", 9],
+    ["prepay", 10],
+    ["account", 11],
+    ["social", 12],
+    ["credit", 13],
+  ]);
+
+  function highlight(para, mp) {
+    let highlightedText = para.replace(/\b\w+\b/g, function(word) {
+      const lowerWord = word.toLowerCase();
+      if (mp.has(lowerWord)) {
+        return `<span style="color:black; background-color: white;">${word}</span>`;
+      } else {
+        return word;
+      }
+    });
+
+    return <span dangerouslySetInnerHTML={{ __html: highlightedText }} />;
+  }
+
   return (
     <div className='outer'>
       <div className='glass'>
         <div className='left'>
-          <img src={itisSpam} />
+          <img src={itisSpam} alt='spam' />
         </div>
         <div className='right'>
-          <p style={{ color: '#FF203F' }}>This Messege is Spam</p>
-          Link Detected
-          Open In Virtual Browser
-          <button>Virtual Brwoser</button>
+          <p style={{ color: '#FF203F' }}>This Message is Spam</p>
+          {highlight(props.str, mp)}
+          <p>Link Detected</p>
+          <p>Open In Virtual Browser</p>
+          <button>Virtual Browser</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Spam
+export default Spam;
