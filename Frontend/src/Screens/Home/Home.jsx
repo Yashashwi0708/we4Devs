d1a10ee568a61d4c1b42e321ab40b4588a1e4bd3
Smit <smit2003.sb@gmail.com>
2024-03-24 04:16:43 +0530

    temp

--- Frontend/src/Screens/Home/Home.jsx
+++ Frontend/src/Screens/Home/Home.jsx
@@ -6,24 +6,23 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-    <div> 
-      <div className="gpt3__header section__padding" id="home">
-        <div className="gpt3__header-content">
-          <h1 className="gradient__text">Better Safe Than Sorry</h1>
-          <p>Feel confident knowing that our advanced security system is diligently analyzing messages and links to protect you from potential threats</p>
-          <div className="gpt3__header-content__input">
+      <div>
+        <div className="gpt3__header section__padding" id="home">
+          <div className="gpt3__header-content">
+            <h1 className="gradient__text">Better Safe Than Sorry</h1>
+            <p className="text2">Feel confident knowing that our advanced security system is diligently analyzing messages and links to protect you from potential threats</p>
+            <div className="gpt3__header-content__input">
             {/* <Link to={'/checkspam'}> */}
               <button type="button">Share to us</button>
-            {/* </Link> */}
+            </div>
+          </div>
+
+          <div className="gpt3__header-image vert-move">
+            <img src={boxLock} />
           </div>
         </div>
-
-        <div className="gpt3__header-image vert-move">
-          <img src={boxLock} />
-        </div>
       </div>
-    </div>
-    
+
     </>
   )
 }
