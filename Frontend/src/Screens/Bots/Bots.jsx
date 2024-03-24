022d9446cd0ed63f9fbe1f5883ddd623f60ccd52
sid-1164 <siddheshkitkaru@gmail.com>
2024-03-24 08:34:16 +0530

    bot page progress

--- Frontend/src/Screens/Bots/Bots.jsx
+++ Frontend/src/Screens/Bots/Bots.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Bots.css'
+import whastapp from '../../../Assets/bots/whastapp-icon.jpg'
+import telegram from '../../../Assets/bots/telegram-icon.jpeg'
+import discord from '../../../Assets/bots/discord-icon.png'
 
 function Bots() {
   return (
+    <div className='container'>
+
+      
+
+      <div className='left'>
+        <img src={whastapp} alt='WhatsApp' />
+        <h1>WhatsApp Bot</h1>
+        <p>Powered by Safe Guard</p>
+      </div>
+      
+      <div className='center'>
+        <img src={telegram} alt='Telegram' />
+        <h1>Telegram Bot</h1>
+        <p>Powered by Safe Guard</p>
+      </div>
+      
+      <div className='right'>
+        <img src={discord} alt='Discord '/>
+        <h1>Discord Bot</h1>
+        <p>Powered by Safe Guard</p>
+      </div>
     
-    <h1>bots</h1>
+    </div>
   )
 }
 
