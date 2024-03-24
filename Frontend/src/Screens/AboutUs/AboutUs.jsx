7530633cf7f6f693883e99e964a2bc0aceeccd20
sid-1164 <siddheshkitkaru@gmail.com>
2024-03-24 11:26:34 +0530

    about us

--- Frontend/src/Screens/AboutUs/AboutUs.jsx
+++ Frontend/src/Screens/AboutUs/AboutUs.jsx
@@ -8,15 +8,16 @@ const AboutUs = () => {
         <div className="gpt3__header section__padding" id="home">
           <div className="gpt3__header-content">
             <h1 className="gradient__text">Hackathon Project</h1>
-            <p className="text2">This website is made as part of Hackathon Project.</p>
-            <p className="text2">For the purpose to ensure security awerness and browser safely</p>
-            <p className="text2">Our Team : </p>
-                <ul className='text2'>
-                    <li>Yashashvi Patil</li>
-                    <li>Jyotiraditya Patil</li>
-                    <li>Smit Butle</li>
-                    <li>Siddhesh Kitkaru</li>
-                </ul>
+            <p className="text2">This website is made as part of Hackathon Project</p>
+            <p className="text2">For the purpose to ensure security awerness and browser safety SafeGaurd has been made</p>
+            <p className="text2">We are altogether the students of Walchand College Of Enginnering, Sangli</p>
+            
+            <p className="text2">We are Team <b>we4Devs</b></p>
+            <p >Yashashwi Patil</p>
+            <p >Jyotiraditya Patil</p>
+            <p >Smit Butle</p>
+            <p >Siddhesh Kitkaru</p>
+
             <div className="gpt3__header-contFeel confident knowing that our advanced security system is diligently analyzing messages and links to protect you from potential threatsent__input">
             </div>
           </div>
