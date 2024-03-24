d3e9b0c650c8669617ae208a104ff8bcaa5ed82e
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2024-03-24 10:28:07 +0530

    Bot.jsx

--- Frontend/src/Screens/Bots/Bots.jsx
+++ Frontend/src/Screens/Bots/Bots.jsx
@@ -1,34 +1,33 @@
 import React from 'react'
 import './Bots.css'
-import whastapp from '../../../Assets/bots/whastapp-icon.jpg'
-import telegram from '../../../Assets/bots/telegram-icon.jpeg'
-import discord from '../../../Assets/bots/discord-icon.png'
+import whastapp from '../../../Assets/bots/whatsapp.png'
+import telegram from '../../../Assets/bots/telegram.png'
+import discord from '../../../Assets/bots/discord.png'
 
 function Bots() {
   return (
-    <div className='container'>
-
-      
-
-      <div className='left'>
-        <img src={whastapp} alt='WhatsApp' />
-        <h1>WhatsApp Bot</h1>
-        <p>Powered by Safe Guard</p>
+    <div className='outer'>
+      <div className='container'>
+        <div className='left'>
+          <img src={whastapp} alt='WhatsApp' />
+          <h1>WhatsApp Bot</h1>
+          <p>Powered by Safe Guard</p>
+          <a href='' target='_blank' rel='noreferrer' style={{color:"white"}}>Add to WhatsApp</a>
+        </div>
+        <div className='center'>
+          <img src={telegram} alt='Telegram' />
+          <h1>Telegram Bot</h1>
+          <p>Powered by Safe Guard</p>
+          <a href='' target='_blank' rel='noreferrer' style={{color:"white"}}>Add to Telegram</a>
+        </div>
+        <div className='right'>
+          <img src={discord} alt='Discord' style={{ scale: "1.2" }} />
+          <h1>Discord Bot</h1>
+          <p>Powered by Safe Guard</p>
+          <a href='' target='_blank' rel='noreferrer' style={{color:"white"}}>Add to Discord</a>
+        </div>
       </div>
-      
-      <div className='center'>
-        <img src={telegram} alt='Telegram' />
-        <h1>Telegram Bot</h1>
-        <p>Powered by Safe Guard</p>
-      </div>
-      
-      <div className='right'>
-        <img src={discord} alt='Discord '/>
-        <h1>Discord Bot</h1>
-        <p>Powered by Safe Guard</p>
-      </div>
-    
-    </div>
+    // </div>
   )
 }
 
